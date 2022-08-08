fd77972a8fbb64e75fcb4b1fda3a247acaa0f797
omm-prakash <ommprakash.sahoo.eee20@itbhu.ac.in>
2022-08-08 20:06:46 +0530

    final commit before merging api branch

--- application/config.py
+++ application/config.py
@@ -10,7 +10,7 @@ class Config():
 class LocalDevelopmentConfig(Config):
     SQLITE_DB_DIR = os.path.join(basedir, "../db_directory")
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(SQLITE_DB_DIR, "database.sqlite3")
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY =  "ash ah secet"
     SECURITY_PASSWORD_HASH = "bcrypt"    
     SECURITY_PASSWORD_SALT = "really super secret" # Read from ENV in your case
